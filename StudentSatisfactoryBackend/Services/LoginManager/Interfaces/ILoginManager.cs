191c1f9f9d2342a3e796e1841b2a1f1e2675b04f
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-09-22 15:17:07 +0200

    Implement Login interface

--- StudentSatisfactoryBackend/Services/LoginManager/Interfaces/ILoginManager.cs
+++ StudentSatisfactoryBackend/Services/LoginManager/Interfaces/ILoginManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentSatisfactoryBackend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace StudentSatisfactoryBackend.Services.LoginManager
 {
     interface ILoginManager
     {
+        public User Login(Object creds);
     }
 }
