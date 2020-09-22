17c7908e77036c0a62971669eea406b0ed7309bd
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-09-22 15:13:53 +0200

    Create LoginManager

---
+++ StudentSatisfactoryBackend/Services/LoginManager/LoginManager.cs
@@ -0,0 +1,40 @@
+﻿using StudentSatisfactoryBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Services.LoginManager
+{
+    public class LoginManager : ILoginManager
+    {
+        private User Register(Object creds)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsAuthenticated(Object creds)
+        {
+            throw new NotImplementedException();
+        }
+        public User Login(Object creds)
+        {
+            bool isAuthenticated = IsAuthenticated(creds);
+            User user;
+
+            if (isAuthenticated)
+            {
+                user = SurveyContext.GetUser(creds);
+
+                if (user == null)
+                {
+                    user = Register(creds);
+                }
+
+                return user;
+            
+               return null;
+            
+        }
+    }
+}
