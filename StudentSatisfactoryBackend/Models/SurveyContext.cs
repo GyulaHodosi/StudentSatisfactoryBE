82459d4a07a4d38da905b9e50d2b2103dcbb907a
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 11:56:33 +0200

    Add Dbsets to context

--- StudentSatisfactoryBackend/Models/SurveyContext.cs
+++ StudentSatisfactoryBackend/Models/SurveyContext.cs
@@ -15,6 +15,17 @@ namespace StudentSatisfactoryBackend.Data
             : base(options) { }
 
         public DbSet<User> RegisteredUsers { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<UserQuestion> UserQuestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserQuestion>()
+                .HasKey(uq => new { uq.UserId, uq.QuestionId });
+
+        }
     }
 
 }
