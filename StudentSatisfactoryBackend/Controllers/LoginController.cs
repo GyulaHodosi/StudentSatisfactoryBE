41c5ac7a19e0d587dbb84d77218c47bceca08f64
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-09-23 11:22:36 +0200

    Implement google authentication

--- StudentSatisfactoryBackend/Controllers/LoginController.cs
+++ StudentSatisfactoryBackend/Controllers/LoginController.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using StudentSatisfactoryBackend.Data;
 using StudentSatisfactoryBackend.Models;
 using StudentSatisfactoryBackend.Services.LoginManager;
+using Microsoft.AspNetCore.Identity;
+using Google.Apis.Auth;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +20,7 @@ namespace StudentSatisfactoryBackend.Controllers
     public class LoginController : Controller
     {
         private readonly ILoginManager _loginManager;
+        private readonly UserManager<User> _userManager;
 
         public LoginController(SurveyContext context)
         {
@@ -31,7 +35,10 @@ namespace StudentSatisfactoryBackend.Controllers
 
         [HttpPost]
         public async Task<ActionResult<User>> Login(string tokenId)
-        { 
+        {
+            Payload payload;
+            User user = null;
+
             if ( !Request.Headers.ContainsKey("X-Requested-With"))
             {
                 return BadRequest();
@@ -39,10 +46,48 @@ namespace StudentSatisfactoryBackend.Controllers
 
             try
             {
-                payload = await VaidateAsync(tokenId)
+                payload = await GoogleJsonWebSignature.ValidateAsync(tokenId,
+                    new ValidationSettings
+                    {
+                        Audience = new[] { Startup._clientData.ClientId }
+                    });
+            } catch (InvalidJwtException e)
+            {
+                return BadRequest();
             }
-            User user = await _loginManager.Login(code);
+
             return user;
+        }
+
+        public async Task<User> GetOrCreateExternalLoginUser(string provider, string key, string email, string firstName, string lastName)
+        {
+            // Login already linked to a user
+            var user = await _userManager.FindByLoginAsync(provider, key);
+            if (user != null)
+                return user;
+
+            user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                // No user exists with this email address, we create a new one
+                user = new User
+                {
+                    Email = email,
+                    UserName = email,
+                    FirstName = firstName,
+                    LastName = lastName
+                };
+
+                await _userManager.CreateAsync(user);
+            }
+
+            // Link the user to this login
+            var info = new UserLoginInfo(provider, key, provider.ToUpperInvariant());
+            var result = await _userManager.AddLoginAsync(user, info);
+            if (result.Succeeded)
+                return user;
+
+            return null;
         }
     }
 }
