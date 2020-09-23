26582c9514c7205e21e2882bbf986a3b0424dc0d
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 11:56:56 +0200

    Create interfaces for Feedback, Question and User repository

---
+++ StudentSatisfactoryBackend/Repositories/Interfaces/IFeedbackRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentSatisfactoryBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Repositories.Interfaces
+{
+    public interface IFeedbackRepository
+    {
+        Task<IEnumerable<Feedback>> GetAllFeedbacks();
+        Task<IEnumerable<Feedback>> GetTop10Feedbacks();
+        Task<IEnumerable<Feedback>> GetAllFeedbacksOfWeek(DateTime date);
+        Task<IEnumerable<Feedback>> GetTop10FeedbacksOfWeek(DateTime date);
+        Task<IEnumerable<Feedback>> ListFeedbacksByUser(string userId);
+        Task<Feedback> GetFeedbackById(int id);
+        void AddFeedback(Feedback feedback);
+    }
+}
