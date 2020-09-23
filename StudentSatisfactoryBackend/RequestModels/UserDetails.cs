13a289c5f31aa36ada030fa5e0549eaa26573593
Máté Iszlai <iszimate@gmail.com>
2020-09-23 12:09:22 +0200

    Create UserDetails class

---
+++ StudentSatisfactoryBackend/RequestModels/UserDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSatisfactoryBackend.RequestModels
+{
+    public class UserDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+        public string UserRole { get; set; }
+    }
+}
