3e02ba2233e1fd55f171a9a9c11d5d1a366b99b2
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-23 11:56:02 +0200

    Add models for Feedback, Question and UserQuestion

---
+++ StudentSatisfactoryBackend/Models/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public int VoteCount { get; set; }
+        public DateTime Date { get; set; }
+
+        public Feedback(string userId, string title)
+        {
+            UserId = userId;
+            Title = title;
+            VoteCount = 0;
+            Date = DateTime.Now;
+        }
+    }
+}
---
+++ StudentSatisfactoryBackend/Models/UserQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Models
+{
+    public class UserQuestion
+    {
+        public User User { get; set; }
+        public string UserId { get; set; }
+        public Question Question { get; set; }
+        public int QuestionId { get; set; }
+        public int Value { get; set; }
+        public string Description { get; set; }
+    }
+}
