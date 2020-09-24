033ed41af6dc062ac95ebc689086c5a26f71517a
Máté Iszlai <iszimate@gmail.com>
2020-09-24 14:13:33 +0200

    Create ICourseRepository interface

---
+++ StudentSatisfactoryBackend/Repositories/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudentSatisfactoryBackend.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Repositories.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<IEnumerable<Course>> GetAllCourse();
+
+    }
+}
