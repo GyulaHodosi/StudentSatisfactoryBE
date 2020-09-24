29755b043c218f5068bb6cce41e5a34a3426fd5d
Máté Iszlai <iszimate@gmail.com>
2020-09-24 15:47:05 +0200

    Create CourseToUser class

---
+++ StudentSatisfactoryBackend/Models/RequestModels/CourseToUser.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentSatisfactoryBackend.Models.RequestModels
+{
+    public class CourseToUser
+    {
+        public string CourseName { get; set; }
+        public string TokenId { get; set; }
+    }
+}
