73fa7f569fe23ee71fa29898267268f5fa0899d0
Máté Iszlai <iszimate@gmail.com>
2020-09-24 14:14:22 +0200

    Create Course controller

---
+++ StudentSatisfactoryBackend/Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentSatisfactoryBackend.Data;
+using StudentSatisfactoryBackend.Repositories;
+using StudentSatisfactoryBackend.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Controllers
+{
+    [Route("api/courses")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseController(SurveyContext context)
+        {
+            _courseRepository = new CourseRepository(context);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetCoureses()
+        {
+            var courses = await _courseRepository.GetAllCourse();
+            if(courses.Count() != 0)
+            {
+                return Ok(courses);
+            }
+            return NotFound();
+        }
+    }
+}
