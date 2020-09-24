1cd4f2951aef406ef6d8b4efd35e4003dc046cb8
Máté Iszlai <iszimate@gmail.com>
2020-09-24 14:14:05 +0200

    Create CourseRepository class

---
+++ StudentSatisfactoryBackend/Repositories/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSatisfactoryBackend.Data;
+using StudentSatisfactoryBackend.Models;
+using StudentSatisfactoryBackend.Repositories.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly SurveyContext _context;
+
+        public CourseRepository(SurveyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Course>> GetAllCourse()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+    }
+}
