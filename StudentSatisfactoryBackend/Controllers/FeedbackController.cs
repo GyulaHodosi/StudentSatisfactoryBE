c32211df93d5f7705bdb49309747db8778eb84f4
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-09-28 15:56:47 +0200

    Add controllers

---
+++ StudentSatisfactoryBackend/Controllers/FeedbackController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentSatisfactoryBackend.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Controllers
+{
+    [Route("feedback")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IFeedbackRepository _repository;
+
+        public FeedbackController(IFeedbackRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllFeedbacks()
+        {
+            try
+            {
+                var feedbacks = await _repository.GetAllFeedbacks();
+                return Ok(feedbacks);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("all/week")]
+        public async Task<IActionResult> GetAllFeedbacksByWeek()
+        {
+            try
+            {
+                var feedbacks = await _repository.GetAllFeedbacksOfWeek(DateTime.Now);
+                return Ok(feedbacks);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return BadRequest();
+            }
+        }
+
+        //[HttpPost]
+        //public async
+    }
+}
