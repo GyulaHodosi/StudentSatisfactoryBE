2384290dd207b59cd2fdae9c0e23f52a1d1cb16f
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-01 13:52:54 +0200

    Finish question repository base methods

--- StudentSatisfactoryBackend/Repositories/QuestionRepository/Interfaces/IQuestionRepository.cs
+++ StudentSatisfactoryBackend/Repositories/QuestionRepository/Interfaces/IQuestionRepository.cs
@@ -11,10 +11,12 @@ namespace StudentSatisfactoryBackend.Repositories.Interfaces
     {
         Task<IEnumerable<Question>> GetAllQuestions();
         Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestion(int questionId);
+        Task<IEnumerable<UserQuestion>> GetAllAnswersOfUser(string userId);
         Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestionByWeek(DateTime date,int questionId);
         Task<Question> GetQuestionById(int id);
-        Task<bool> AddQuestion(Question question);
-        Task<bool> EditQuestion(Question question);
-        void DeleteQuestion(Question question);
+        Task<bool> AddQuestion(string title);
+        Task<bool> EditQuestion(int id, string title);
+        Task<bool> DeleteQuestion(int questionId);
+        Task<bool> AddAnswer(string title, string userId, int questionId, int value);
     }
 }
--- StudentSatisfactoryBackend/Repositories/QuestionRepository/QuestionRepository.cs
+++ StudentSatisfactoryBackend/Repositories/QuestionRepository/QuestionRepository.cs
@@ -18,46 +18,6 @@ namespace StudentSatisfactoryBackend.Repositories
         {
             _context = context;
         }
-        public async Task<bool> AddQuestion(Question question)
-        {
-            var newQuestion = new Question(question.Title);
-            _context.Questions.Add(newQuestion);
-            try
-            {
-                await _context.SaveChangesAsync();
-                return true;
-            }
-            catch (DbUpdateException)
-            {
-
-                return false;
-            }
-        }
-
-        public void DeleteQuestion(Question question)
-        {
-            _context.Questions.Remove(question);
-        }
-
-        public async Task<bool> EditQuestion(Question question)
-        {
-            var _question = await _context.Questions.SingleOrDefaultAsync(q => q.Id == question.Id);
-            //TODO
-            throw new NotImplementedException();
-        }
-
-        public async Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestion(int questionId)
-        {
-            var userQuestion = await _context.UserQuestions.Where(uq => uq.QuestionId == questionId).ToListAsync();
-            return userQuestion;
-        }
-
-        public async Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestionByWeek(DateTime date, int questionId)
-        {
-            var userQuestion = await _context.UserQuestions.Where(uq => uq.QuestionId == questionId).ToListAsync();
-            //TODO
-            throw new NotImplementedException();
-        }
 
         public async Task<IEnumerable<Question>> GetAllQuestions()
         {
@@ -69,6 +29,101 @@ namespace StudentSatisfactoryBackend.Repositories
         {
             var question = _context.Questions.FirstOrDefaultAsync(q => q.Id == id);
             return question;
+        }
+
+        public async Task<bool> AddQuestion(string title)
+        {
+            try
+            {
+                var newQuestion = new Question(title);
+                _context.Questions.Add(newQuestion);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteQuestion(int questionId)
+        {
+            try
+            {
+                var question = await _context.Questions.SingleOrDefaultAsync(q => q.Id == questionId);
+                _context.Questions.Remove(question);
+
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> EditQuestion(int id, string title)
+        {
+            try
+            {
+                var _question = await _context.Questions.SingleOrDefaultAsync(q => q.Id == id);
+                _question.Title = title;
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestion(int questionId)
+        {
+            var userQuestions = await _context.UserQuestions.Where(uq => uq.QuestionId == questionId).ToListAsync();
+            return userQuestions;
+        }
+
+        public async Task<IEnumerable<UserQuestion>> GetAllAnswersOfUser(string userId)
+        {
+            var userQuestions = await _context.UserQuestions.Where(uq => uq.UserId == userId).ToListAsync();
+            return userQuestions;
+        }
+
+        public async Task<IEnumerable<UserQuestion>> GetAllAnswersOfQuestionByWeek(DateTime date, int questionId)
+        {
+            var userQuestion = await _context.UserQuestions.Where(uq => uq.QuestionId == questionId).ToListAsync();
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> AddAnswer(string description, string userId, int questionId, int value)
+        {
+            var response = new UserQuestion()
+            {
+                Description = description,
+                QuestionId = questionId,
+                UserId = userId,
+                Value = value
+            };
+
+            try
+            {
+                _context.UserQuestions.Add(response);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
         }
     }
 }
