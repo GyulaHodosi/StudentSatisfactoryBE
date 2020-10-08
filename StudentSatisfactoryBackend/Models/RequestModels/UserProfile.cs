f8cc1ff7f6c3ddbe137d46e05f7f28c046afb8eb
Máté Iszlai <iszimate@gmail.com>
2020-10-08 16:25:16 +0200

    Remove unnecessary usings

--- StudentSatisfactoryBackend/Models/RequestModels/UserProfile.cs
+++ StudentSatisfactoryBackend/Models/RequestModels/UserProfile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace StudentSatisfactoryBackend.Models.RequestModels
+﻿namespace StudentSatisfactoryBackend.Models.RequestModels
 {
     public class UserProfile
     {
