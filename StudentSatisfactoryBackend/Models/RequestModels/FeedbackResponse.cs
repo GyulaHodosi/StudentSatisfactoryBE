6c7009c098f55e88f4fc6e28be5bed4914f124bd
Máté Iszlai <iszimate@gmail.com>
2020-10-08 15:42:17 +0200

    Create FeedbackResponse class

---
+++ StudentSatisfactoryBackend/Models/RequestModels/FeedbackResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StudentSatisfactoryBackend.Models.RequestModels
+{
+    public class FeedbackResponse
+    {
+        public IEnumerable<FeedbackToSend> Feedbacks { get; set; }
+        public IEnumerable<int> VotedFeedbackIds { get; set; }
+    }
+}
