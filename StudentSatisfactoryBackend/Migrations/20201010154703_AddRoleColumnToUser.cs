e23498bf8c9cc9a0c58cb0e07712d734cc48b94f
Máté Iszlai <iszimate@gmail.com>
2020-10-10 17:54:25 +0200

    Add migrations and AdminEmails table to database

---
+++ StudentSatisfactoryBackend/Migrations/20201010154703_AddRoleColumnToUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentSatisfactoryBackend.Migrations
+{
+    public partial class AddRoleColumnToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AdminEmails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminEmails", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AdminEmails",
+                columns: new[] { "Id", "Email" },
+                values: new object[] { 1, "codecool.satisafactionapp@gmail.com" });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminEmails");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+
+        }
+    }
+}
