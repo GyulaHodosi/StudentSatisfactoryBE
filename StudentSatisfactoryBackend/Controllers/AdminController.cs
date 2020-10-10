3c2a7e09f7cae31f37af30cc7627e6caf806bfd8
Máté Iszlai <iszimate@gmail.com>
2020-10-10 17:53:42 +0200

    Create AdminController

---
+++ StudentSatisfactoryBackend/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentSatisfactoryBackend.Repositories.AdminRepository;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Controllers
+{
+    [Route("api/admins")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public AdminController(IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAdmin([FromBody] string email)
+        {
+            if (await _adminRepository.AddAdminRoleByEmail(email))
+                return Ok();
+            return StatusCode(500);
+        }
+    }
+}
