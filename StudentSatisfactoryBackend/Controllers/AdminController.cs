44f40c2bbccf1ea0f5207571cf648d9ff874cd88
Máté Iszlai <iszimate@gmail.com>
2020-10-10 18:07:03 +0200

    Add IsAdmin route

--- StudentSatisfactoryBackend/Controllers/AdminController.cs
+++ StudentSatisfactoryBackend/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentSatisfactoryBackend.Models.RequestModels;
 using StudentSatisfactoryBackend.Repositories.AdminRepository;
+using StudentSatisfactoryBackend.Repositories.UserRepository.Interfaces;
 using System.Threading.Tasks;
 
 namespace StudentSatisfactoryBackend.Controllers
@@ -9,18 +11,31 @@ namespace StudentSatisfactoryBackend.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IAdminRepository _adminRepository;
+        private readonly IUserRepository _userRepository;
 
-        public AdminController(IAdminRepository adminRepository)
+        public AdminController(IAdminRepository adminRepository, IUserRepository userRepository)
         {
             _adminRepository = adminRepository;
+            _userRepository = userRepository;
         }
 
         [HttpPost]
         public async Task<IActionResult> AddAdmin([FromBody] string email)
         {
             if (await _adminRepository.AddAdminRoleByEmail(email))
-                return Ok();
+                return Created("Email successfully added", "");
             return StatusCode(500);
+        }
+
+        [HttpPost("isadmin")]
+        public async Task<IActionResult> IsAdmin(LoginData data)
+        {
+            var user = await _userRepository.GetUserByTokenId(data.TokenId);
+            if (user == null || user.Role == "user")
+            {
+                return NotFound();
+            }
+            return Ok();
         }
     }
 }
