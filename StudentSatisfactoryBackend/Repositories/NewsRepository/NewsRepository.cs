af171bc0d6bc48c9bef65b2ddb4f82c113324525
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-11 18:40:29 +0200

    Implement NewsRepository

---
+++ StudentSatisfactoryBackend/Repositories/NewsRepository/NewsRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentSatisfactoryBackend.Data;
+using StudentSatisfactoryBackend.Models;
+using StudentSatisfactoryBackend.Repositories.NewsRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Repositories.NewsRepository
+{
+    public class NewsRepository : INewsRepository
+    {
+        private readonly SurveyContext _context;
+
+        public NewsRepository(SurveyContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IEnumerable<News>> GetAllNews()
+        {
+            var news = await _context.News.ToListAsync();
+            return news;
+        }
+
+        public async Task<News> GetNewsById(int id)
+        {
+            var news = await _context.News.FirstOrDefaultAsync(n => n.Id == id);
+            return news;
+        }
+        public async Task<bool> AddNews(string userId, string userName, string description, DateTime date)
+        {
+            try
+            {
+                var newPost = new News(userId, userName, description, date);
+                _context.News.Add(newPost);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+        }
+    }
+}
