8d4a8a7677a155a57d2ff2beb9e57bf4439f9a0a
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-11 18:40:16 +0200

    Create NewsRepository interface

---
+++ StudentSatisfactoryBackend/Repositories/NewsRepository/Interfaces/INewsRepository.cs
@@ -0,0 +1,15 @@
+﻿using StudentSatisfactoryBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Repositories.NewsRepository.Interfaces
+{
+    public interface INewsRepository
+    {
+        Task<IEnumerable<News>> GetAllNews();
+        Task<News> GetNewsById(int id);
+        Task<bool> AddNews(string userId, string userName, string description, DateTime date);
+    }
+}
