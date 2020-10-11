a26773221bc555cae068b3029bc493f117c4f912
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-11 15:45:23 +0200

    Add AverageOfAnswers class

---
+++ StudentSatisfactoryBackend/Models/AverageOfAnswers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Models
+{
+    public class AverageOfAnswers
+    {
+        public Report Report { get; set; }
+        public int ReportId { get; set; }
+        public Question Question { get; set; }
+        public int QuestionId { get; set; }
+        public int Average { get; set; }
+
+        public AverageOfAnswers(int reportId, int questionId, int average)
+        {
+            ReportId = reportId;
+            QuestionId = questionId;
+            Average = average;
+        }
+    }
+}
