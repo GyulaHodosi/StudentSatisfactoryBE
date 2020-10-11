f121f4a75ee4bd6cea871925f1277483fbcc3580
Izabella Dolezsár <izabella.dolezsar@gmail.com>
2020-10-11 15:20:06 +0200

    Create Report class

---
+++ StudentSatisfactoryBackend/Models/Report.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSatisfactoryBackend.Models
+{
+    public class Report
+    {
+        public int SurveyId { get; set; }
+        public int FillCount { get; set; }
+        public Dictionary<int, int> QuestionAverage { get; set; }
+
+        public Report(int surveyId, List<UserQuestion> answers)
+        {
+            SurveyId = surveyId;
+            QuestionAverage = new Dictionary<int, int>();
+            FillCount = GetFillCount(answers);
+            AddAveragesOfQuestions(answers);
+        }
+
+        public void AddAveragesOfQuestions(List<UserQuestion> answers)
+        {
+            foreach(var answer in answers)
+            {
+                if (QuestionAverage.ContainsKey(answer.QuestionId))
+                {
+                    QuestionAverage[answer.QuestionId] += answer.Value;
+                } else
+                {
+                    QuestionAverage[answer.QuestionId] = answer.Value;
+                }
+            }
+
+            foreach(var key in QuestionAverage.Keys)
+            {
+                QuestionAverage[key] = QuestionAverage[key] / FillCount;
+            }
+        }
+
+        private int GetFillCount(List<UserQuestion> answers)
+        {
+            return answers.Count(a => a.QuestionId == 1 && a.SurveyId == SurveyId);
+
+        }
+    }
+}
