52d6b7064f49ff8e3708ae20cb001fbee63fe702
Máté Iszlai <iszimate@gmail.com>
2020-10-19 11:51:08 +0200

    Write migrate database

--- StudentSatisfactoryBackend/Extension/ServiceExtensions.cs
+++ StudentSatisfactoryBackend/Extension/ServiceExtensions.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentSatisfactoryBackend.Data;
+using System;
 
 namespace StudentSatisfactoryBackend.Extension
 {
@@ -27,6 +28,23 @@ namespace StudentSatisfactoryBackend.Extension
             {
 
             });
+        }
+
+        public static IHost MigrateDatabase(this IHost webHost)
+        {
+            // Manually run any pending migrations if configured to do so.
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (env == "Production")
+            {
+                var serviceScopeFactory = (IServiceScopeFactory)webHost.Services.GetService(typeof(IServiceScopeFactory));
+                using (var scope = serviceScopeFactory.CreateScope())
+                {
+                    var services = scope.ServiceProvider;
+                    var dbContext = services.GetRequiredService<SurveyContext>();
+                    dbContext.Database.Migrate();
+                }
+            }
+            return webHost;
         }
     }
 }
